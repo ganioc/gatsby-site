ec824d1c33621f587b3c48254e781ffed04e57ea
yango3 <o3.yang@hotmail.com>
2021-11-10 17:44:55 +0800

    update README.md update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,7 +131,7 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
+        数据分发共享组件演示页面
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
